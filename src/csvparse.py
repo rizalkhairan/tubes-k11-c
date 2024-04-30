e415f76f8931e499afa9b8cef9930023b5e46151
Syahrizal Khairan <19623260@std.stei.itb.ac.id>
2024-04-30 12:16:12 +0700

    feat: add write_csv procedure

--- src/csvparse.py
+++ src/csvparse.py
@@ -2,6 +2,9 @@
 
     Fungsi
         parse_csv(file) -> [[str]]  : memproses file csv dalam bentuk array dua dimensi
+    
+    Prosedur
+        write_csv(table, file)      : menulis table menjadi file csv
 """
 
 from split import split
@@ -19,6 +22,17 @@ def parse_csv(file):
             placehold += char
 
     return table
+
+def write_csv(table, file):
+    for row in table:
+        line = ''
+        for column in row:
+            line += column
+            line += ';'
+        line = line[:-1] + '\n'
+
+        file.write(line)
+            
 
 # ==================== Test ====================
 
